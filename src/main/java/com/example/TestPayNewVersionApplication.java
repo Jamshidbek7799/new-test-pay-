5a97ee33622668af03fa901f169a6abe75435c2e
Jamshidbek7799 <jamshidyuldashev7799@gmail.com>
2021-12-21 21:40:37 +0500

    new test pay project added

---
+++ src/main/java/com/example/TestPayNewVersionApplication.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestPayNewVersionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestPayNewVersionApplication.class, args);
+    }
+
+}
